68e503f805028219e821c1cc87dbfd1bf5aee508
Dylan C. <dylan.castillo.asu@gmail.com>
2025-04-30 07:17:58 -0700

    created LoginRequestDTO.java

---
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -0,0 +1,47 @@
+package com.pm.authservice.util;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final Key secretKey;
+
+    //Constructor method basically setting the jwt secret from environment variables and set it a Key object
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        byte[] keyBytes = Base64.getDecoder().decode(secret.getBytes(StandardCharsets.UTF_8));
+
+        this.secretKey = Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public String generateToken(String email, String role) {
+        return Jwts.builder()
+                .subject(email)
+                .claim("role", role)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) //10 hours set to 10mins once done with testing
+                .signWith(secretKey)
+                .compact();
+    }
+
+    public void validateToken(String token) {
+        try{
+            Jwts.parser().verifyWith((SecretKey) secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+        } catch (JwtException e){
+            throw new JwtException("Invalid JWT token");
+        }
+    }
+
+}
